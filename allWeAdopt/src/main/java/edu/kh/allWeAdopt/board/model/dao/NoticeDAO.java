fb55aac0a5f2c93f11e6a2b100978428604ae8bf
song-jh <thdwpgml11@naver.com>
2022-07-11 15:00:31 +0900

    공지사항 조회 수정

--- allWeAdopt/src/main/java/edu/kh/allWeAdopt/board/model/dao/NoticeDAO.java
+++ allWeAdopt/src/main/java/edu/kh/allWeAdopt/board/model/dao/NoticeDAO.java
@@ -2,23 +2,42 @@ package edu.kh.allWeAdopt.board.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import edu.kh.allWeAdopt.board.model.vo.Board;
+import edu.kh.allWeAdopt.board.model.vo.Pagination;
 
+@Repository
 public class NoticeDAO {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+	
+	/** 공지사항 게시글 수 조회 DAO
+	 * @return listCount
+	 */
+	public int getListCount() {
+		
+		return sqlSession.selectOne("noticeMapper.getListCount");
+	}
 
 	/** 공지사항 목록 조회 DAO
-	 * @param cp
-	 * @return
+	 * @param pagination
+	 * @return nList
 	 */
-	public List<Board> selectNoticeList(int cp) {
+	public List<Board> selectNoticeList(Pagination pagination) {
 		
-		return null;
+		int offset = ( pagination.getCurrentPage()-1) * pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		return sqlSession.selectList("boardMapper.selectBoardList", rowBounds);
+		
 	}
+
+
 
 }
