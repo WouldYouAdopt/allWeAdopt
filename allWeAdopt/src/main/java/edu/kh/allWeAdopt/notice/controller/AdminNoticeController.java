e6528362fd7cee98dea5b280e677c34fb3492286
song-jh <thdwpgml11@naver.com>
2022-07-11 11:45:14 +0900

    admin.controller에서 notice.controller로 패키지 변경

--- allWeAdopt/src/main/java/edu/kh/allWeAdopt/notice/controller/AdminNoticeController.java
+++ allWeAdopt/src/main/java/edu/kh/allWeAdopt/notice/controller/AdminNoticeController.java
@@ -1,33 +1,46 @@
-package edu.kh.allWeAdopt.admin.controller;
+package edu.kh.allWeAdopt.notice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import edu.kh.allWeAdopt.notice.model.service.NoticeService;
+
 @Controller
 @RequestMapping("/admin/notice")
 @SessionAttributes({"loginMember"})
 public class AdminNoticeController {
+	
+	@Autowired
+	private NoticeService service;
+	
+	private Logger logger = LoggerFactory.getLogger(AdminNoticeController.class);
 
+	// 관리자 - 공지사항 리스트 조회화면
 	@GetMapping("/list")
 	public String adminNoticeList() {
+		
+		logger.info("공지사항 리스트 조회 수행됨");
 		
 		return "notice/noticeList";
 	}
 	
+	// 관리자 - 공지사항 상세조회화면
 	@GetMapping("/detail")
 	public String adminNoticeDetail() {
 		
 		return "notice/noticeDetail";
 	}
 	
+	// 관리자 - 공지사항 작성화면
 	@GetMapping("/write")
 	public String adminNoticeWrite() {
 		
 		return "notice/noticeWrite";
 	}
-	
-	
 	
 }
