fbc8138e8f160e6706f1c62eecd757afd4387f9f
SJ-HM <su2168kr@naver.com>
2022-07-13 11:20:51 +0900

    전단지 목록 조회, 작성화면

--- allWeAdopt/src/main/java/edu/kh/allWeAdopt/shelter/controller/ShelterController.java
+++ allWeAdopt/src/main/java/edu/kh/allWeAdopt/shelter/controller/ShelterController.java
@@ -2,6 +2,9 @@ package edu.kh.allWeAdopt.shelter.controller;
 
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,7 +12,12 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FileUtils;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
@@ -22,10 +30,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.google.gson.JsonObject;
 
+import edu.kh.allWeAdopt.board.model.vo.Board;
+import edu.kh.allWeAdopt.shelter.model.service.ShelterReplyService;
 import edu.kh.allWeAdopt.shelter.model.service.ShelterService;
 import edu.kh.allWeAdopt.shelter.model.vo.Shelter;
 
@@ -37,12 +50,15 @@ public class ShelterController {
 	@Autowired
 	private ShelterService service;
 	
+	@Autowired
+	private ShelterReplyService replyService;
+	
 	private Logger logger = LoggerFactory.getLogger(ShelterController.class);
 
 	
 	
 	// 보호소 불러오기
-	@GetMapping("/mainShelter")
+	@GetMapping("/shelterList")
 	public String openShelter(Model model, @RequestParam(value="pageNo", required=false, defaultValue = "1") String pageNo,
 								@RequestParam(value="upkind", required=false, defaultValue = "")String upkind, 
 								@RequestParam(value="upr_cd", required=false, defaultValue = "") String upr_cd
@@ -146,7 +162,7 @@ public class ShelterController {
 	    model.addAttribute("upr_cd", upr_cd);	    
 //	    model.addAttribute("org_cd", org_cd);	    
 
-		return "shelter/mainShelter";
+		return "shelter/shelter-main";
 	}
 	
 	// 보호소 상세 조회
@@ -155,7 +171,96 @@ public class ShelterController {
 		
 //		System.out.println(desertionNo);
 		
-		return "shelter/shelterDetail";
+		return "shelter/shelter-Detail";
+	}
+	
+	
+	
+	// 전단지 목록 조회 
+	@GetMapping("/pamphletList")
+	public String pamphletList(@RequestParam(value = "cp", required = false, defaultValue = "1") int cp, Model model, 
+			@RequestParam Map<String, Object> paramMap) {
+		
+		
+		Map<String , Object> map = null;
+		
+		if(paramMap.get("query") == null) { // 검색이 아닌 경우
+			map = service.selectPamphletList(cp);
+			
+		}else { // 검색인 경우
+			
+			// 검색에 필요한 데이터를 paramMap에 모두 담아서 서비스 호출
+			// -> key, query, cp, boardCode
+			
+			paramMap.put("cp", cp); // 있으면 같은 값으로 덮어쓰기, 없으면 추가		
+			
+//			 map = service.searchPamphletList(paramMap);
+			
+		}
+		
+		model.addAttribute("map", map);
+		
+		
+		return "shelter/shelter-pamphlet";
+	}
+	
+	
+	// 전단지 작성 화면
+	@GetMapping("/pamphletWrite")
+	public String pamphletWriteForm(String mode, @RequestParam(value="no", required=false, defaultValue="0") int boardNo, Model modell) {
+		
+//		if(mode.equals("update")) {
+//			// 게시글 상세조회 서비스 호출
+//			BoardDetail detail = service.selectBoardDetail(boardNo);
+//			// -> 개행문자가 <br>로 되어있는 상태 -> textarea 출력 예정이기 때문에 \n으로 변경
+//			
+//			detail.setBoardContent(Util.newLineClear(detail.getBoardContent()));
+//			
+//			model.addAttribute("detail", detail);
+//		}
+		
+		return "shelter/shelter-pamphletWrite";
+		
+	}
+	
+	
+//	// 전단지 작성(삽입/수정)
+//	public String pamphletWrite(Board board, )
+	
+	
+	// 파일 업로드
+	@RequestMapping(value="/user/uploadSummernoteImageFile", produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String uploadSummernoteImageFil(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest req ) {
+		
+		JsonObject jsonObject = new JsonObject();
+		
+		String webPath = "/resources/images/board/";
+		
+		// 컴퓨터 상에서 webPath까지의 실제 경로를 불러옴
+		String fileRoot = req.getSession().getServletContext().getRealPath(webPath);
+		
+		String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
+		String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
+		String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
+		
+		File targetFile = new File(fileRoot + savedFileName);	
+		try {
+			InputStream fileStream = multipartFile.getInputStream();
+			FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
+			jsonObject.addProperty("url", "/resources/images/board/"+savedFileName); // contextroot + resources + 저장할 내부 폴더명
+			jsonObject.addProperty("responseCode", "success");
+				
+		} catch (IOException e) {
+			FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
+			jsonObject.addProperty("responseCode", "error");
+			e.printStackTrace();
+		}
+		
+		String image = jsonObject.toString();
+		
+		return image;
+		
 	}
 	
 	
--- allWeAdopt/src/main/java/edu/kh/allWeAdopt/shelter/model/dao/ShelterDAO.java
+++ allWeAdopt/src/main/java/edu/kh/allWeAdopt/shelter/model/dao/ShelterDAO.java
@@ -1,10 +1,16 @@
 package edu.kh.allWeAdopt.shelter.model.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import edu.kh.allWeAdopt.board.model.vo.Board;
+import edu.kh.allWeAdopt.board.model.vo.Pagination;
 
 @Repository
 public class ShelterDAO {
@@ -13,5 +19,26 @@ public class ShelterDAO {
 	private SqlSessionTemplate sqlSession;
 	
 	private Logger logger = LoggerFactory.getLogger(ShelterDAO.class);
+
+	/** 전단지 수 조회
+	 * @return int
+	 */
+	public int getListCount() {
+		return sqlSession.selectOne("pamphletMapper.getListCount");
+	}
+
+	
+	/** 전단지 목록 조회
+	 * @param pagination
+	 * @return
+	 */
+	public List<Board> selectPamphletList(Pagination pagination) {
+		
+		int offset = ( pagination.getCurrentPage()-1) * pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		return sqlSession.selectList("pamphletMapper.selectPamphletList", null, rowBounds);
+	}
 	
 }
